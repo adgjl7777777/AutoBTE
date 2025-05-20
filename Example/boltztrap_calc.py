76f237b2572e3ae41e0a8c8870e88afc91e0917e
adgjl7777777 <adgjl7777777@naver.com>
2025-05-20 14:59:42 +0900

    fixed multiple calculation

--- Example/boltztrap_calc.py
+++ Example/boltztrap_calc.py
@@ -3,6 +3,7 @@ bte_run(
     output_dir="./test",
     vasp_dir="./vasp",
     fixed_temp=300,
+    equivalence=20000,
     fixed_mu=0.0,
     plot_type=['dos','band_structure','electric_conductivity', 'seebeck', 'thermal_conductivity', 'powfactor', 'zT']
 )
